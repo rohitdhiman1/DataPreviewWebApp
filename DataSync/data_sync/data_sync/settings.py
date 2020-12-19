a29126e9887b947c7747d5b8b8de19c0f563cbc3
Rohit Dhiman <rohitdhiman0202@gmail.com>
2020-12-19 12:49:16 +0530

    Updated settings

--- DataSync/data_sync/data_sync/settings.py
+++ DataSync/data_sync/data_sync/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'zp-yw&crkxo-k(+^%zrw@@rxn31)$&x97gg7*4pl^b@-6*2+%v'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
